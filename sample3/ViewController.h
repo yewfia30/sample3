c5b8921ded10915b1bfcf2434ca4208acf506568
yewfia30 <yewfia30@gmail.com>
2016-03-24 21:55:50 +0900

    Initial Commit

---
+++ sample3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sample3
+//
+//  Created by yoshinobu on 2016/03/24.
+//  Copyright © 2016年 testorg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
